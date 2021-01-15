c959c10ceff14b249f4b1f0631e0269739ee4095
nrkoehler <norbert.r.koehler@gmail.com>
2021-01-15 21:21:41 +0100

    ADD paste_lipsum()

--- R/helpers.R
+++ R/helpers.R
@@ -151,7 +151,6 @@ NULL
 #' (in R Markdown documents)}
 #' @param x  number
 #' @param rev  FALSE (default) for increasing trend, TRUE for decreasing trend (reduction).
-#' @examples
 #' @export
 more_less <- function(x, rev = FALSE) {
   if (rev == FALSE) {
@@ -210,6 +209,26 @@ pad_var <- function(x, prefix = NULL) {
   x <- stringr::str_pad(x, width = z, side = "left", pad = "0")
   x <- paste0(prefix, x)
   x
+}
+NULL
+#' @title {Print 'Lorem Ipsum'}
+#' @description {Print 'Lorem Ipsum' to the console or
+#' in R Markdown documents}
+#' @param n The number of lorem ipsum paragraphs to be printed
+#' @source {\url{https://github.com/aakosm/lipsum}}
+#' @export
+paste_lipsum <- function(n = 1){
+  ipsum <- paste("Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
+                 "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 
+                 "Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                 "laboris nisi ut aliquip ex ea commodo consequat.", 
+                 "Duis aute irure dolor in reprehenderit in voluptate velit",
+                 "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint",
+                 "occaecat cupidatat non proident, sunt in culpa qui officia",
+                 "deserunt mollit anim id est laborum.")
+  for (i in 1:n) {
+    cat(ipsum, sep = "\n")
+  }
 }
 NULL
 #' @title {Shorten character string}
